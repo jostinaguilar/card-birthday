bf2abbf0ffda9c033952a626d34851d659b17f35
Jostin Aguilar <aguilarjostin2001@gmail.com>
2023-09-24 23:58:49 -0500

    add animations v1

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import confetti from "canvas-confetti";
+import anime from "animejs";
 
 function adjustScreen() {
   const card = document.querySelector(".card");
@@ -10,6 +11,45 @@ function adjustScreen() {
 window.addEventListener("DOMContentLoaded", () => {
   adjustScreen();
 
+  anime({
+    targets: ".name",
+    scale: 0,
+    direction: "reverse",
+    easing: "easeInElastic(1, .6)",
+  });
+
+  anime({
+    targets: ".birth",
+    scale: 0,
+    direction: "reverse",
+    delay: 200,
+    endDelay: 200,
+    easing: "easeInElastic(1, .6)",
+  });
+
+  anime({
+    targets: ".one",
+    scale: 0,
+    direction: "reverse",
+    delay: 400,
+    endDelay: 400,
+    easing: "easeInElastic(1, .6)",
+  });
+
+  anime({
+    targets: ".guest",
+    opacity: 0,
+    direction: "reverse",
+    easing: "easeInOutSine",
+  });
+
+  anime({
+    targets: [".zenon", ".pollito", ".bartolito"],
+    translateY: 200,
+    direction: "reverse",
+    easing: "easeInOutSine",
+  });
+
   setTimeout(() => {
     confetti({
       particleCount: 150,
@@ -19,7 +59,7 @@ window.addEventListener("DOMContentLoaded", () => {
       scalar: 0.8,
       //colors: ["#780CF6", "#300BD4", "#1728EB", "#0B50D4", "#0C9BF6"],
     });
-  }, 400);
+  }, 500);
 });
 
 window.addEventListener("resize", adjustScreen);
